d27ea765aeff0dc5faa4e299a99af4ff161c523e
Nikhil Ganta <nikhilganta95@gmail.com>
2021-01-31 22:36:31 -0700

    Mock Data Commented

--- app.py
+++ app.py
@@ -377,14 +377,14 @@ def search_artists():
   # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
   # seach for "A" should return "Guns N Petals", "Matt Quevado", and "The Wild Sax Band".
   # search for "band" should return "The Wild Sax Band".
-  response={
-    "count": 1,
-    "data": [{
-      "id": 4,
-      "name": "Guns N Petals",
-      "num_upcoming_shows": 0,
-    }]
-  }
+  # response={
+  #   "count": 1,
+  #   "data": [{
+  #     "id": 4,
+  #     "name": "Guns N Petals",
+  #     "num_upcoming_shows": 0,
+  #   }]
+  # }
   search_term=request.form.get('search_term', '')
   artists = Artist.query.filter(Artist.name.ilike("%{}%".format(search_term))).all()
   response = {
